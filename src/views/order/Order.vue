4cc6cea6919ccdd28402fea026eee69fafefa3d6
wdd <2979554858@qq.com>
2023-05-07 21:47:30 +0800

    增加订单退款

--- src/views/order/Order.vue
+++ src/views/order/Order.vue
@@ -20,6 +20,8 @@
             <el-option value="2" label="已发货"></el-option>
             <el-option value="3" label="已完成"></el-option>
             <el-option value="4" label="已取消"></el-option>
+            <el-option value="5" label="退款中"></el-option>
+            <el-option value="6" label="退款成功"></el-option>
 					</el-select>  
 					<el-button slot="append" icon="el-icon-search" @click="search"></el-button>
 				</el-input >
